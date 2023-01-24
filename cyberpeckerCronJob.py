1e45f8a28d0f14c39b4358d1ae438a2147f5e95e
hitesh <hitesh22rana@gmail.com>
2023-01-24 19:07:19 +0530

    cronjobs changes and syntax updates

--- cyberpeckerCronJob.py
+++ cyberpeckerCronJob.py
@@ -4,25 +4,26 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class cyberPeckerCronJob:
+class CyberPeckerCronJob:
 
     def __init__(self) -> None:
         self.session = requests.session()
-        self.BASE_URL = os.getenv("BASE_URL")
+        self.URL = os.getenv("BASE_URL")
         self.WORKERS = 4
 
-    def _getNewsQueries(self):
-        return self.session.get(self.BASE_URL, timeout=10).json()
+    @property
+    def __get_news_queries(self):
+        return self.session.get(self.URL, timeout=10).json()
 
-    def _getNewsRoute(self):
-        data = self._getNewsQueries()
+    def _get_news_route(self):
+        data = self.__get_news_queries
 
         routes = []
-        for individualLink in data:
-            route = list(individualLink.keys())[0]
-            routes.append(self.BASE_URL + route)
+        for individual_link in data:
+            route = list(individual_link.keys())[0]
+            routes.append(self.URL + route)
 
         return routes
 
-    def getNewsResponse(self, route):
+    def get_news_response(self, route):
         self.session.get(route, timeout=10)
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from deta import app
 import time
 import concurrent.futures
-from cyberpeckerCronJob import cyberPeckerCronJob
+from CyberpeckerCronJob import CyberPeckerCronJob
 
 @app.lib.cron()
-def cronTask(event):
-    cronJob = cyberPeckerCronJob()
-    routes = cronJob._getNewsRoute()
+def cron_task():
+    cron_job: CyberPeckerCronJob = CyberPeckerCronJob()
+    routes: list = cron_job._get_news_route()
 
-    totalTimeStart = time.time()
+    total_time_start: float = time.time()
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=cronJob.WORKERS) as executor:
-        futures = {executor.submit(cronJob.getNewsResponse, route) for route in routes}
+    with concurrent.futures.ThreadPoolExecutor(max_workers=cron_job.WORKERS) as executor:
+        futures = {executor.submit(cron_job.get_news_response, route) for route in routes}
         concurrent.futures.wait(futures)
 
-    totalTimeEnd = time.time()        
+    total_time_end = time.time()        
     
-    return f'Successfully completed cronjob in : {round(totalTimeEnd - totalTimeStart, 2)}s'
+    return f'Successfully completed cronjob in : {round(total_time_end - total_time_start, 2)}s'
